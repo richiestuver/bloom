75ac8a36a65bb0e352adde4f4e8ec17639ba3d99
Richie Stuver <stuverr@oregonstate.edu>
2023-03-03 22:44:47 -0500

    remove sqrt call

--- sieve.c
+++ sieve.c
@@ -35,10 +35,10 @@ int main(int argc, char* argv[])
     int arg_idx = parse_opts(argc, argv);
 
     unsigned long long N = strtoull(argv[arg_idx], NULL, 10);
-    unsigned long long sqrt_N = (unsigned long long)(floor(sqrt(N)));
+    // unsigned long long sqrt_N = (unsigned long long)(floor(sqrt(N)));
 
     if (VERBOSE) {
-        printf("N: %llu, sqrtN: %llu\n", N, sqrt_N);
+        printf("N: %llu\n", N);
     }
 
     unsigned long long* arr = calloc(N, sizeof(unsigned long long));
@@ -49,7 +49,7 @@ int main(int argc, char* argv[])
     arr[0] = 1;
     arr[1] = 1;
 
-    for (unsigned long long i = 2; i <= sqrt_N; i++) {
+    for (unsigned long long i = 2; pow(i, 2) <= N; i++) {
         if (arr[i] == 0) {
 
             unsigned long long k = 2;
